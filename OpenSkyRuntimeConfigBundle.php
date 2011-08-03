96afb65f1daef2a00b2c250c692f2b22fd04dd8d
Jeremy Mikola <jmikola@gmail.com>
2011-08-03 13:10:57 -0400

    Manually load extension to overcome naming convention requirement in Bundle::getContainerExtension()

--- OpenSkyRuntimeConfigBundle.php
+++ OpenSkyRuntimeConfigBundle.php
@@ -2,8 +2,13 @@
 
 namespace OpenSky\Bundle\RuntimeConfigBundle;
 
+use OpenSky\Bundle\RuntimeConfigBundle\DependencyInjection\OpenSkyRuntimeConfigExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OpenSkyRuntimeConfigBundle extends Bundle
 {
+    public function __construct()
+    {
+        $this->extension = new OpenSkyRuntimeConfigExtension();
+    }
 }
